ed8c21a8c2618d6e85b3c370dc54e59ed50727fb
wu <1757171296@qq.com>
2022-02-25 20:36:26 +0800

    将顶栏和左栏合并前的版本

--- js/test.js
+++ js/test.js
@@ -4,6 +4,7 @@ window.onload=function(){
     // console.log(dropdown);
     // console.log(dropdownLi);
     for(let i=0;i<dropdownLi.length;i++){
+        // 因为2,3,4没有效果所以不添加
         if(i>0&&i<4) continue;
             dropdownLi[i].onmouseover=function(){
                 this.classList.add("show");
