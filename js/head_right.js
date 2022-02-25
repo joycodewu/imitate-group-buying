6dfcdc9ccd122f6b917c68d8a9a1f6f4796b1876
wu <1757171296@qq.com>
2022-02-25 21:16:13 +0800

    添加了侧栏，侧栏位置还未修正前的版本

--- js/head_right.js
+++ js/head_right.js
@@ -1,8 +1,8 @@
 window.onload=function(){
     var dropdown=document.getElementById("dropdown");
     var dropdownLi=dropdown.getElementsByTagName("li");
-    // console.log(dropdown);
-    // console.log(dropdownLi);
+    console.log(dropdown);
+    console.log(dropdownLi);
     for(let i=0;i<dropdownLi.length;i++){
         // 因为2,3,4没有效果所以不添加
         if(i>0&&i<4) continue;
@@ -14,25 +14,3 @@ window.onload=function(){
             }
     }
 }
-
-
-window.onload=function(){
-    var dropright=document.getElementById("dropright");
-    var droprightLi=dropright.getElementsByTagName("li");
-    console.log(dropright);
-    console.log(droprightLi);
-    for(let i=0;i<droprightLi.length;i++){
-        if(i!=0) continue;
-        droprightLi[i].onmouseover=function(){
-            this.classList.add("show");
-        }
-        droprightLi[i].onmouseout=function(){
-            this.classList.remove("show");
-        }
-    }
-
-
-
-
-}
-
