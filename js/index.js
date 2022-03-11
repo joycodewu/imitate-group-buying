47d48ab4b6e234b8df76f3edb81ece38998f1931
wu <1757171296@qq.com>
2022-03-11 12:13:21 +0800

    首页顶栏城市的效果完成

--- js/index.js
+++ js/index.js
@@ -227,3 +227,9 @@ var maoyanBoxBody=document.getElementById("maoyan-box-body");
         }
     }
 }
+//获取city页面传值过来的城市信息
+function GetQueryString(name){
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r!=null)return  decodeURIComponent(r[2]); return null;
+}
